5b56b261b67c0bda0ddc88bffe90b71416c3434e
Jayrel Chaudhary <jssmith@email.neit.edu>
2022-12-07 13:35:35 -0500

    End of class Week 9 day 2

--- AsteriodAvoider/AsteriodAvoider.js
+++ AsteriodAvoider/AsteriodAvoider.js
@@ -1,10 +1,11 @@
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d");
 var timer = requestAnimationFrame(main);
-var gameOver = false;
+var gameOver = true;
 var score = 0;
 var highScore = 0;
-
+var currentState = 0;
+var gameState = [];
 //asteriod variables
 var numAsteroids = 20;
 var asteroids = [];
@@ -50,6 +51,32 @@ function pressKeyDown(e) {
         if (e.keyCode == 40) {
             //code for down down
             ship.down = true;
+        }
+    }
+
+    // menu inputs use spacebar
+    if(gameOver){
+        if(e.keyCode == 32){
+            if(currentState ==2){
+                //game over input
+                currentState = 0;
+                numAsteroids = 20;
+                score = 0;
+                
+            }else{
+                //main menu screen
+                currentState = 1;
+            gameOver = false;
+            scoreTimer();
+            main();
+            gameStart();
+            }
+          
+          
+          
+          
+          
+            
         }
     }
 }
@@ -128,11 +155,35 @@ function PlayerShip() {
     this.right = false;
     this.vx = 0;
     this.vy = 0;
+    this.flameLength = 30;
+   
 
 
     this.drawShip = function () {
         ctx.save();
         ctx.translate(this.x, this.y);
+
+        //Draw the thruster
+        if(this.up || this.left || this.right){
+            ctx.save();
+            if(this.flameLength == 30){
+                this.flameLength = 20;
+                ctx.fillStyle = "yellow"
+            }else{
+                this.flameLength = 30;
+                ctx.fillStyle = "orange";
+            }
+            //Draw the Flame
+            ctx.beginPath();
+            ctx.moveto(0, this,this.flameLength);
+            ctx.lineTo(5, 5);
+            ctx.lineTo(-5, 5);
+            ctx.lineTo(0, this.flameLength);
+            ctx.closePath();
+            ctx.fill();
+            ctx.restore();
+        }
+
 
         //draw ship
         ctx.fillStyle = "red";
@@ -186,77 +237,146 @@ function PlayerShip() {
 
 
 
-//for loop to instantiate asteroids for game
-for (var i = 0; i < numAsteroids; i++) {
-    asteroids[i] = new Asteriod();
-}
+
 
 function main() {
     //clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    //draw score to screen
-    ctx.save();
-    ctx.font = "15px Arial";
-    ctx.fillStyle = "white";
-    ctx.fillText("Score: " + score.toString(), canvas.width - 150, 30);
-    ctx.restore();
-
-    //vertical movemnet 
-    if (ship.up) {
-        ship.vy = -5;
-    } else {
-        ship.vy = 5;
-    }
-    //horizontal movement
-    if (ship.left) {
-        ship.vx = -5;
-    } else if (ship.right) {
-        ship.vx = 5;
-    } else {
-        ship.vx = 0;
-    }
-
-    for (var i = 0; i < asteroids.length; i++) {
-        var dX = ship.x - asteroids[i].x;
-        var dY = ship.y - asteroids[i].y;
-        var distance = Math.sqrt((dX * dX) + (dY * dY));
-
-        if (detectCollision(distance, (ship.height / 2 + asteroids[i].radius))) {
-            //console.log("Hit Astroid")
-            //alert("hit asteroid")
-            gameOver = true
-
-        }
-
-        if (asteroids[i].y > canvas.height + asteroids[i].radius) {
-            asteroids[i].y = randomRange(canvas.height - asteroids[i].radius, asteroids[i].radius) - canvas.height;
-            asteroids[i].x = randomRange(canvas.width - asteroids[i].radius, asteroids[i].radius)
-        }
-
-        asteroids[i].y += asteroids[i].vy;
-        asteroids[i].drawAsteroid();
-
-
-    }
-
-    //draw the ship
-    ship.moveShip();
-    ship.drawShip();
-
+   
+    gameState[currentState]();
     if (!gameOver) {
         //refresh the screen
         timer = requestAnimationFrame(main);
     }
 
-    while(asteroids.length < numAsteroids){
-        asteroids.push(new Asteriod());
-    }
+   
 
 
 }
 
+//Game state machine
+
+//Main Menu State
+gameState[0] = function(){
+    //code for main menu
+    ctx.save();
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("Asteroid Avoider", canvas.width/2, canvas.height/2 - 30);
+    ctx.font = "15px Arial";
+    ctx.fillText("Press Space to Start", canvas.width/2, canvas.height/2);
+    ctx.restore();
+}
+
+//Play game State
+gameState[1] = function(){
+    //code for astriods game
+     //draw score to screen
+     ctx.save();
+     ctx.font = "15px Arial";
+     ctx.fillStyle = "white";
+     ctx.fillText("Score: " + score.toString(), canvas.width - 150, 30);
+     ctx.restore();
+ 
+     //vertical movemnet 
+     if (ship.up) {
+         ship.vy = -5;
+     } else {
+         ship.vy = 5;
+     }
+     //horizontal movement
+     if (ship.left) {
+         ship.vx = -5;
+     } else if (ship.right) {
+         ship.vx = 5;
+     } else {
+         ship.vx = 0;
+     }
+ 
+     for (var i = 0; i < asteroids.length; i++) {
+         var dX = ship.x - asteroids[i].x;
+         var dY = ship.y - asteroids[i].y;
+         var distance = Math.sqrt((dX * dX) + (dY * dY));
+
+        //collisison detection happends here
+         if (detectCollision(distance, (ship.height / 2 + asteroids[i].radius))) {
+             //console.log("Hit Astroid")
+             //alert("hit asteroid")
+             gameOver = true;
+             currentState = 2;
+             main();
+             return;
+ 
+         }
+ 
+         if (asteroids[i].y > canvas.height + asteroids[i].radius) {
+             asteroids[i].y = randomRange(canvas.height - asteroids[i].radius, asteroids[i].radius) - canvas.height;
+             asteroids[i].x = randomRange(canvas.width - asteroids[i].radius, asteroids[i].radius)
+         }
+ 
+         asteroids[i].y += asteroids[i].vy;
+         asteroids[i].drawAsteroid();
+ 
+ 
+     }
+ 
+     //draw the ship
+     ship.moveShip();
+     ship.drawShip();
+     
+     //
+     while(asteroids.length < numAsteroids){
+        asteroids.push(new Asteriod());
+    }
+}
+
+//Game Over State
+gameState[2] = function(){
+    if(score > highScore){
+        highScore = score;
+
+        ctx.save();
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "white"
+        ctx.textAlign = "center";
+        ctx.fillText("Game Over, Your Score was  " + score.toString(), canvas.width/2, canvas.height/2 - 60);
+        ctx.fillText("Your New High Score is  " + highScore.toString(), canvas.width/2, canvas.height/2 - 30);
+        ctx.fillText("New Record!",canvas.width/2, canvas.height/2);
+        ctx.font = "15px Arial";
+        ctx.fillText("Press space to play again", canvas.width/2, canvas.height/2 + 20);
+        ctx.restore();
+    }else{
+        ctx.save();
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "white"
+        ctx.textAlign = "center";
+        ctx.fillText("Game Over, Your Score was  " + score.toString(), canvas.width/2, canvas.height/2 - 60);
+        ctx.fillText("Your New High Score is  " + highScore.toString(), canvas.width/2, canvas.height/2 - 30);
+        ctx.fillText("New Record!",canvas.width/2, canvas.height/2);
+        ctx.font = "15px Arial";
+        ctx.fillText("Press space to play again", canvas.width/2, canvas.height/2 + 20);
+        ctx.restore();
+    }
+
+
+
+    //code for game over menu
+   
+}
+
+
 //Utility functions
+
+function gameStart(){
+    //for loop to instantiate asteroids for game
+for (var i = 0; i < numAsteroids; i++) {
+    asteroids[i] = new Asteriod();
+}
+
+    ship = new PlayerShip();
+}
 
 function randomRange(high, low) {
     return Math.random() * (high - low) + low;
@@ -280,4 +400,4 @@ function scoreTimer(){
 }
 
 // temp call score function
-scoreTimer();
+//scoreTimer();
